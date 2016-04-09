a8ae6679275363927d00bf3329b531bba104a195
Fernando <koynonia@gmail.com>
2016-04-08 23:26:49 -0300

    Correção em métodos

--- POOTarde/src/observer/Pessoa.java
+++ POOTarde/src/observer/Pessoa.java
@@ -1,15 +1,13 @@
-package Observer;
+package observer;
 
 public class Pessoa implements Subscriber {
-
 	private String nome;
 	public Pessoa() {
 	}
 	public Pessoa(String nome) {
 		this.setNome(nome);
 	}
-	@Override
-	public void updates(String msg) {
+	public void update(String msg) {
 		System.out.println(nome + " recebeu a seguinte noticia " + msg);
 	}
 	public String getNome() {
